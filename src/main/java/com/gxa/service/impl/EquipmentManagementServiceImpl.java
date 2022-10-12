3917c3f470fe23e71a6e64b07e738d9f39844dea
fengyin <fengyin025@163.com>
2022-10-12 08:51:46 +0800

    测试swagger

--- src/main/java/com/gxa/service/impl/EquipmentManagementServiceImpl.java
+++ src/main/java/com/gxa/service/impl/EquipmentManagementServiceImpl.java
@@ -3,7 +3,6 @@ package com.gxa.service.impl;
 import com.gxa.entity.EquipmentManagement;
 import com.gxa.mapper.EquipmentManagementMapper;
 import com.gxa.service.EquipmentManagementService;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
