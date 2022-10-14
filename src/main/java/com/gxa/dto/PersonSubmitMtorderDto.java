4045da4bb143f6de361fcde8c11200ea261dcbd9
liuhao <1083634910@qq.com>
2022-10-14 19:59:56 +0800

    刘浩   人员管理

--- src/main/java/com/gxa/dto/PersonSubmitMtorderDto.java
+++ src/main/java/com/gxa/dto/PersonSubmitMtorderDto.java
@@ -1,6 +1,7 @@
 package com.gxa.dto;
 
 import com.gxa.entity.KeepPlan;
+import com.gxa.entity.KeepRecord;
 import com.gxa.entity.Personal;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -11,8 +12,8 @@ import java.util.List;
 @Data
 @ApiModel("人员抽组数据类型")
 public class PersonSubmitMtorderDto {
-    @ApiModelProperty(value = "已选人员",name = "personals")
-    private List<Personal> personals;
-    @ApiModelProperty(value = "已选任务",name = "keepPlans")
-    private List<KeepPlan> keepPlans;
+    @ApiModelProperty(value = "已选维修人员id",name = "personalId")
+    private List<Integer> personalId;
+    @ApiModelProperty(value = "已选待维修任务任务编号",name = "keepRecordId")
+    private List<Integer> keepRecordId;
 }
