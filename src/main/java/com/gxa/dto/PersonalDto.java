01dc47ac0011bdb9edd5e8b0300b62142b991dab
liuhao <1083634910@qq.com>
2022-10-19 16:07:16 +0800

    刘浩1

--- src/main/java/com/gxa/dto/PersonalDto.java
+++ src/main/java/com/gxa/dto/PersonalDto.java
@@ -5,16 +5,18 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
-@ApiModel("人员查询条件")
-public class PersonalQueryDto {
+@ApiModel("人员抽组人员信息搜索")
+public class PersonalDto {
     @ApiModelProperty(value = "页码",name = "page")
     private Integer page;
     @ApiModelProperty(value = "每页记录数",name = "limit")
     private Integer limit;
     @ApiModelProperty(value = "姓名",name = "pname")
     private String pname;
-    @ApiModelProperty(value = "部门id",name = "pdeptId")
-    private String pdeptId;
-    @ApiModelProperty(value = "工种id",name = "pjobId")
-    private String pjobId;
+    @ApiModelProperty(value = "部门Id",name = "pdeptId")
+    private Integer pdeptId;
+    @ApiModelProperty(value = "工种Id",name = "pjobId")
+    private Integer pjobId;
+    @ApiModelProperty(value = "工种等级Id",name = "pclassId")
+    private Integer pclassId;
 }
